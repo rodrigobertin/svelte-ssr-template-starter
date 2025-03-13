1754b130591c3080c2cb3c81d54c1a3311c2f47b
RNB-mac-mini <rodrigonbertin@gmail.com>
2025-03-13 20:27:36 -0300

    Correcciones menores

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,5 @@
 <script lang="ts">
-    import { View } from 'svelte-pilot';
+  import {View} from 'svelte-pilot';
 </script>
 
-<main>
-
-  <View/>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-
-</style>
+<View/>
